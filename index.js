61a08075fd5af548f5eb7b773c9df9f9143929d3
tmannazy <e.anene@hotmail.com>
2022-09-08 16:12:54 +0100

    Create a function to serve different pages

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-// const eventEmitter = require("events")
-// const
-
-const { log } = require("console");
 const http = require("http");
 const fs = require("fs").promises;
 const host = "localhost";
 const port = 8080;
 
 const requestListener = (req, res) => {
-  fs.readFile(__dirname)
-    .then((file) => {
-      res.setHeader("Content-Type", "text/html");
-      res.writeHead(200);
-      let filePath;
-      switch (req.url) {
-        case "/index.html":
-          filePath = path.join(file, "/index.html");
-          break;
-        case "/about.html":
-          filePath = path.join(file, "/about.html");
-          break;
-        case "/contact-me.html":
-          filePath = path.join(file, "/contact-me.html");
-          break;
-        case "/404.html":
-          filePath = path.join(file, "/404.html");
-          break;
-      }
-      res.end(filePath);
-    })
-    .catch((err) => {
-      res.statusCode = 500;
-      res.end(err.message);
-      console.log("Error: " + err.message);
-      return;
-    });
+  function serve(pathToFile) {
+    fs.readFile(pathToFile)
+      .then((file) => {
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(200);
+        res.end(file);
+      })
+      .catch((err) => {
+        res.statusCode = 500;
+        res.end(err.message);
+        return;
+      });
+  }
+
+  let filePath;
+  switch (req.url) {
+    case "/":
+      // filePath = path.join(__dirname, "/index.html");
+      filePath = __dirname + "/index.html";
+      serve(filePath);
+      break;
+    case "/about":
+      filePath = __dirname + "/about.html";
+      serve(filePath);
+      break;
+    case "/contact":
+      filePath = __dirname + "/contact-me.html";
+      serve(filePath);
+      break;
+    case "/404":
+      filePath = __dirname + "/404.html";
+      serve(filePath);
+      break;
+  }
 };
 
 const server = http.createServer(requestListener);
