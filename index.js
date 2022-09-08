73c7f0a58704571cb4b731f2ba406111ff2cebca
tmannazy <e.anene@hotmail.com>
2022-09-08 15:01:05 +0100

    Create switch cases to serve different html pages

--- index.js
+++ index.js
@@ -8,12 +8,26 @@ const host = "localhost";
 const port = 8080;
 
 const requestListener = (req, res) => {
-  fs.readFile(__dirname + "/about.html")
+  fs.readFile(__dirname)
     .then((file) => {
       res.setHeader("Content-Type", "text/html");
       res.writeHead(200);
-      res.end(file);
-      console.log("File loaded!");
+      let filePath;
+      switch (req.url) {
+        case "/index.html":
+          filePath = path.join(file, "/index.html");
+          break;
+        case "/about.html":
+          filePath = path.join(file, "/about.html");
+          break;
+        case "/contact-me.html":
+          filePath = path.join(file, "/contact-me.html");
+          break;
+        case "/404.html":
+          filePath = path.join(file, "/404.html");
+          break;
+      }
+      res.end(filePath);
     })
     .catch((err) => {
       res.statusCode = 500;
